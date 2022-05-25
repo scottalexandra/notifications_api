a446b98bc9aec72da9ad44026d7c08228c9b7dbc
Alex Robinson <alexandra.scott335@gmail.com>
2022-05-25 17:02:42 -0600

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create(full_name: "Sevitri Wilson")
+user = User.create(full_name: "Sandra Bullock")
 user.notifications.create(content: "Your articles of incorporation are complete!")
 user.notifications.create(content: "Your application for EIN has been successfully completed")
-user.notifications.create(content: "Your application for EIN has been successfully completed")
+user.notifications.create(content: "Your application for 501c3 Status has been initiated!")
